74f0e325349ba420a1501173334b84650a5b064c
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-21 14:26:10 +0200

    message

---
+++ 4-cours/Animal.php
@@ -0,0 +1,10 @@
+<?php
+class Animal
+{
+    const MAMMIFERE = "chien";
+
+    public function afficheAnimal()
+    {
+        echo "l'animal est un chien ";
+    }
+}
---
+++ 4-cours/index.php
@@ -0,0 +1,6 @@
+<?php
+require("Animal.php");
+echo Animal::MAMMIFERE;
+
+$animal = new Animal();
+$animal->afficheAnimal();
