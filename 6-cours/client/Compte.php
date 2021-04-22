fa7db9e8d78152f7ec69a23e2f67c9904a0d6f99
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-22 15:11:17 +0200

    message

--- 6-cours/client/Compte.php
+++ 6-cours/client/Compte.php
@@ -2,9 +2,22 @@
 
 namespace App\client;
 
+use App\banque\CompteEpargne;
+
 /**
  * Compte client
  */
 class Compte
 {
+    public $nom;
+    public $prenom;
+    public $age;
+    public $CompteEpargne;
+    public function __construct(string $nom, string $prenom, int $age, CompteEpargne $compteE)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->noagem = $age;
+        $this->compteEpargne = $compteE;
+    }
 }
