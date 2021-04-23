0c52d17ac86885d9305bd0cfa7cd7d2b459f2d64
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-23 11:29:10 +0200

    mise en place autoload

---
+++ 9-cours/Voiture.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+class Voiture
+{
+    public $marque;
+}
---
+++ 9-cours/index.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Table;
+use App\Voiture;
+
+require "table.php";
+require "Voiture.php";
+
+$table = new Table();
+$voiture = new Voiture();
